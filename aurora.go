39446840628a207e0280fbc02cc759873b0f0cd0
kiweezi <46600706+kiweezi@users.noreply.github.com>
2023-07-09 16:35:50 +0000

    Add remove function

--- aurora.go
+++ aurora.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
 )
@@ -14,39 +16,74 @@ func main() {
 	// Constructs the client object.
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	ctx := context.Background()
-	// Run a mongo container.
-	RunServer(ctx, cli)
+
+	// Run an nginx container.
+	server, err := RunServer(ctx, cli)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Gets containers that are actively running.
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{
+		Filters: filters.NewArgs(filters.Arg("id", server.ID)),
+	})
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// Outputs running containers.
 	for _, container := range containers {
 		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
 	}
+
+	// Removes the container.
+	err = RemoveServer(ctx, cli, server.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func RunServer(ctx context.Context, cli *client.Client) {
+// Creates a server container and starts it. Similar to `docker run`.
+func RunServer(ctx context.Context, cli *client.Client) (container.CreateResponse, error) {
 	// Create the container.
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
+	response, err := cli.ContainerCreate(ctx, &container.Config{
 		Image:        "nginx",
 		ExposedPorts: nat.PortSet{"8080": struct{}{}},
 	}, &container.HostConfig{
 		PortBindings: map[nat.Port][]nat.PortBinding{nat.Port("8080"): {{HostIP: "127.0.0.1", HostPort: "8080"}}},
 	}, nil, nil, "nginx-go-cli")
 	if err != nil {
-		panic(err)
+		return response, err
 	}
 
 	// Start the container.
-	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		panic(err)
+	if err := cli.ContainerStart(ctx, response.ID, types.ContainerStartOptions{}); err != nil {
+		return response, err
 	}
+
+	return response, nil
+}
+
+// Stops and removes a server container.
+func RemoveServer(ctx context.Context, cli *client.Client, containerID string) error {
+	// Stop and delete the container and volumes.
+	if err := cli.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         true,
+	}); err != nil {
+		return err
+	}
+
+	// Remove unused data.
+	_, err := cli.ContainersPrune(ctx, filters.Args{})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
