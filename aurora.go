6b7a54016e0f38b4c032596cf1f3c95710fc37fb
SamMWoods <58216049+SamMWoods@users.noreply.github.com>
2023-08-06 21:29:14 +0100

    forgot to uncomment your stuff.. ;P

--- aurora.go
+++ aurora.go
@@ -1,76 +1,85 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"ricochet/aurora/api"
+	"ricochet/aurora/docker"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
 
 func main() {
-	// ctx := context.Background()
+	ctx := context.Background()
 
-	// // Constructs the client object.
-	// cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer cli.Close()
+	// Constructs the client object.
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
 
 	// Start the API.
 	api.Start()
 
-	// // // connect to firebase
+	// connect to firebase
 	// app, err := db.FireBase()	
 
-	// // connect to firebase realtime database
+	// connect to firebase realtime database
 	// client, err := db.RealtimeDatabase()
 
-	// // connect to firebase firestore
+	// connect to firebase firestore
 	// firestore, err := db.Firestore()
 
-	// // Create container environment variables.
-	// env, err := docker.NewContainerEnvVar("name", "value")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// // Create container environment ports.
-	// port, err := nat.NewPort("tcp", "8080")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Create container environment variables.
+	env, err := docker.NewContainerEnvVar("name", "value")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create container environment ports.
+	port, err := nat.NewPort("tcp", "8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// // Create container config.
-	// config, err := docker.NewContainerConfig(
-	// 	"my-unique-id",
-	// 	"nginx",
-	// 	nat.PortSet{port: struct{}{}},
-	// 	[]string{"/data:/data"},
-	// 	[]string{env},
-	// )
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Create container config.
+	config, err := docker.NewContainerConfig(
+		"my-unique-id",
+		"nginx",
+		nat.PortSet{port: struct{}{}},
+		[]string{"/data:/data"},
+		[]string{env},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// // Run an nginx container.
-	// server, err := docker.RunServer(ctx, config)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Run an nginx container.
+	server, err := docker.RunServer(ctx, config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// // // Gets containers that are actively running.
-	// // containers, err := cli.ContainerList(ctx, types.ContainerListOptions{
-	// // 	Filters: filters.NewArgs(filters.Arg("id", server.ID)),
-	// // })
-	// // if err != nil {
-	// // 	log.Fatal(err)
-	// // }
+	// Gets containers that are actively running.
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{
+		Filters: filters.NewArgs(filters.Arg("id", server.ID)),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// // // Outputs top 10 running containers.
-	// // for _, container := range containers {
-	// // 	fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-	// // }
+	// Outputs top 10 running containers.
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
 
-	// // Removes the container.
-	// err = docker.RemoveServer(ctx, server.ID)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Removes the container.
+	err = docker.RemoveServer(ctx, server.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
